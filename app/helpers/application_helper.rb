4dc63d927dea5da5e26b759000519c6f9af65f24
Larry Adames <larry@leestapp.com>
2016-02-29 01:13:18 -0500

    formatted song partial, also included audio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def convert_to_duration milliseconds
+    seconds = milliseconds/1000
+    minutes = seconds/60
+    seconds = seconds - (minutes * 60)
+    seconds = "0#{seconds}" unless seconds > 9
+    "#{minutes}:#{seconds} "
+  end
 end
